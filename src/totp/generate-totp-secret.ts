d24827b288889a261061ad0131ea4f2d1580ffc9
Vicentesan <vikom.sanchez@gmail.com>
2025-05-17 19:35:55 -0300

    chore: depreciate a method

--- src/totp/generate-totp-secret.ts
+++ src/totp/generate-totp-secret.ts
@@ -7,6 +7,7 @@ import { base32Encode } from './base-32-encode'
  *
  * @param length - Length of the secret (default: 20 bytes)
  * @returns Base32 encoded secret compatible with authenticator apps
+ * @deprecated We recommend creating your own secrets patterns, for example, combining a two imutable data sources, like a UUID and a timestamp.
  */
 export function generateTOTPSecret({
   length = 20,
